52a843792ae83d050d76b4ef905b627d225eacf3
ar-saad <arsaad.dev@gmail.com>
2025-09-14 16:09:01 +0600

    hosted site on firebase

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -4,22 +4,22 @@ import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
 
 const Dashboard = () => {
-    return (
-        <div
-            className="border-8 h-screen overflow-auto"
-            style={{ borderRadius: "2.5rem", borderColor: "#344679" }}
-        >
-            <div className="grid grid-cols-12">
-                <div className="col-span-2">
-                    <Sidebar />
-                </div>
-                <div className="col-span-10">
-                    <Navbar />
-                    <Outlet />
-                </div>
-            </div>
+  return (
+    <div
+      className="border-8 h-screen overflow-auto"
+      style={{ borderColor: "#344679" }}
+    >
+      <div className="grid grid-cols-12">
+        <div className="col-span-2">
+          <Sidebar />
         </div>
-    );
+        <div className="col-span-10">
+          <Navbar />
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
